51f9f1c0b5ba2eeb696090e6a07ece9fef49c8f8
Brian Dominic Rego <brian.rego03@gmail.com>
2026-02-08 23:52:03 +0530

    Complete controller setup

--- src/main/java/spring/ai/example/book_review_rag/controller/RagController.java
+++ src/main/java/spring/ai/example/book_review_rag/controller/RagController.java
@@ -1,8 +1,23 @@
 package spring.ai.example.book_review_rag.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import spring.ai.example.book_review_rag.service.RagService;
 
 @RestController
 public class RagController {
+    private final RagService ragService;
+    public RagController(RagService ragService) {
+        this.ragService = ragService;
+    }
+
+    @PostMapping("/ai/rag")
+    public String generate(@RequestBody MessageRequest messageRequest){
+        return ragService.retrieveAndGenerate(messageRequest.message());
+
+    }
+
+    public static record MessageRequest(String message){}
 
 }
