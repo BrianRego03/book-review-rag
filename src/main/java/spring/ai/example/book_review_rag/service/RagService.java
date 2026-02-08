38c7cd00ad7fd4e7dd62f8c272ae0e197ec1965a
Brian Dominic Rego <brian.rego03@gmail.com>
2026-02-08 23:24:56 +0530

    Create constructor for the RAG service

--- src/main/java/spring/ai/example/book_review_rag/service/RagService.java
+++ src/main/java/spring/ai/example/book_review_rag/service/RagService.java
@@ -1,7 +1,17 @@
 package spring.ai.example.book_review_rag.service;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RagService {
+    private final ChatClient chatClient;
+    private final VectorStore vectorStore;
+
+    public RagService(ChatClient chatClient, VectorStore vectorStore) {
+        this.chatClient = chatClient;
+        this.vectorStore = vectorStore;
+    }
+
 }
