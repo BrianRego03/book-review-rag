a56a5a947efa6a18e53e95541aef39cc96975061
Brian Dominic Rego <brian.rego03@gmail.com>
2026-02-08 22:58:37 +0530

    Parse the htmlcontent and create a new document with resulting DTO

--- src/main/java/spring/ai/example/book_review_rag/infrastructure/DocumentLoader.java
+++ src/main/java/spring/ai/example/book_review_rag/infrastructure/DocumentLoader.java
@@ -1,9 +1,12 @@
 package spring.ai.example.book_review_rag.infrastructure;
 
+import org.jsoup.Jsoup;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import spring.ai.example.book_review_rag.dto.ParsedReview;
 import spring.ai.example.book_review_rag.ingestion.FileScanner;
 import spring.ai.example.book_review_rag.ingestion.HtmlReader;
 import spring.ai.example.book_review_rag.service.MiniReviewParseService;
@@ -13,6 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+@Component
 public class DocumentLoader implements CommandLineRunner {
     private final VectorStore vectorStore;
     private final MiniReviewParseService miniReviewParseService;
@@ -36,7 +40,16 @@ public class DocumentLoader implements CommandLineRunner {
 
         for (Path path : fileList) {
             String htmlContent = HtmlReader.readHtmlFile(path);
+            ParsedReview reviewObj = miniReviewParseService.parse(htmlContent);
 
+            Document doc = new Document(reviewObj.getText(),reviewObj.getMetadata());
+
+            System.out.println("---- DOCUMENT ----");
+            System.out.println("TEXT:");
+            System.out.println(doc.getText());
+            System.out.println("METADATA:");
+            System.out.println(doc.getMetadata());
+            System.out.println("------------------");
         }
 
 //        documents.add(new Document("Friends Marilyn Hacker and Karthika Nair began a correspondence in verse during " +
