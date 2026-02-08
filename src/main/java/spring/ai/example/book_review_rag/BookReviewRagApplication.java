7e81b1624f1cd1143306559e32a04af2b12faf5f
Brian Dominic Rego <brian.rego03@gmail.com>
2026-02-08 20:24:08 +0530

    Initial commit

---
+++ src/main/java/spring/ai/example/book_review_rag/BookReviewRagApplication.java
@@ -0,0 +1,13 @@
+package spring.ai.example.book_review_rag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReviewRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReviewRagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring/ai/example/book_review_rag/BookReviewRagApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.ai.example.book_review_rag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
