2623d7a5b9f25da674268a60969c7593f55ac3a8
Brian Dominic Rego <brian.rego03@gmail.com>
2026-02-08 23:38:58 +0530

    Use similarity search to fetch the top matching documents

--- src/main/java/spring/ai/example/book_review_rag/service/RagService.java
+++ src/main/java/spring/ai/example/book_review_rag/service/RagService.java
@@ -1,17 +1,46 @@
 package spring.ai.example.book_review_rag.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.SystemPromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RagService {
     private final ChatClient chatClient;
     private final VectorStore vectorStore;
 
+    @Value("classpath:/prompts/rag-prompt.st")
+    private Resource RagTemplate;
+
+
     public RagService(ChatClient chatClient, VectorStore vectorStore) {
         this.chatClient = chatClient;
         this.vectorStore = vectorStore;
+    }
+
+    public String retrieveAndGenerate(String message) {
+        List<Document> similarDocs = vectorStore
+                .similaritySearch(SearchRequest
+                        .builder()
+                        .query(message)
+                        .topK(5)
+                        .build());
+        System.out.println("similarDocs: " + similarDocs);
+        String information = similarDocs
+                .stream()
+                .map(Document::getText)
+                .collect(Collectors.joining("\n"));
+
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(RagTemplate);
+
     }
 
 }
