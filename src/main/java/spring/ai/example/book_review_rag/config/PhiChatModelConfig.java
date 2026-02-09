71ad37407d6442477c631f1cbd6f6de614971bbf
Brian Dominic Rego <brian.rego03@gmail.com>
2026-02-10 01:35:04 +0530

    Build a chat model for phi 3 mini

---
+++ src/main/java/spring/ai/example/book_review_rag/config/PhiChatModelConfig.java
@@ -0,0 +1,24 @@
+package spring.ai.example.book_review_rag.config;
+
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaChatOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PhiChatModelConfig {
+
+    @Bean
+    public OllamaChatModel phiChatModel(OllamaApi ollamaApi) {
+        return OllamaChatModel.builder()
+                .ollamaApi(ollamaApi)
+                .defaultOptions(
+                        OllamaChatOptions
+                                .builder()
+                                .model("phi3:latest")
+                                .build()
+                )
+                .build();
+    }
+}
